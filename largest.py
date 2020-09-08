dc1814bfe6cc165edd45533dc5e0f23ea4fa03ac
Elin Björck <elinbjorck@gmail.com>
2020-09-08 10:59:58 +0200

    klar.

--- largest.py
+++ largest.py
@@ -1,8 +1,14 @@
 integers = [0, 0, 0]
 letters = ['A', 'B', 'C'] #kommer spara i listor ocxh iterera över dem. kanske är onödigt när det är så få, men går jag så här kan jag lätt lägga till fler. 
 largest = 0
+
 print('Please input 3 integers A, B, C')
+
 for i, letter in enumerate(letters):
     integers[i] = int( input(f'Enter {letter}: '))
 
+for i, number in enumerate(integers):
+    if number > largest:
+        largest = number
 
+print(f'Largest number is:  {largest}')
