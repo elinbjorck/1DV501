ef6dbf3b58d4e1b0396dbf42f6449b978b2bc10f
Elin Björck <elinbjorck@gmail.com>
2020-09-08 14:14:12 +0200

    Klar med allt, gött.

--- Assignment 1/squarecolor.py
+++ Assignment 1/squarecolor.py
@@ -1,8 +1,14 @@
-identyfyer = input('Enter a chess square identifyer (from a1 to h8)')
-letter = identyfyer[0]
-number = int(identyfyer[1])
+identifier = input('Enter a chess square identifyer (from a1 to h8)')
+letter = identifier[0]
+number = int(identifier[1]) #här i början kan man sätta in checkar för att kolla så att användaren skriver in en ruta som faktiskt finns på brädet. 
 
-distance_from_a = ord(letter)-ord(a)+1 # Will find the letters distance from a, will give numbers from 0 to 7, then we ad 1 to get numbers from 1 to 8
+letter_number = ord(letter)-ord('a')+1 # Will find the letters distance from a, will give numbers from 0 to 7, 
+                                         #then we ad 1 to get numbers from 1 to 8
 
-if distance_from_a % 2 == number % 2:
-    prin
+black = letter_number % 2 == number % 2 #skriver man om bokstäverna till siffror (a:h)->(1:8) ser man att på de svarta 
+                                          #rutorna är tecknen på siffrorna samma. På de vita rutorna är tecknen olika
+
+if black:
+    print(f'{identifier} is black')
+else:
+    print(f'{identifier} is white')
