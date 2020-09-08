c39406c7491ab8d4a97b6ea95eb85f3a15166c91
Elin Björck <elinbjorck@gmail.com>
2020-09-08 10:34:11 +0200

    fixade så att den printar olika ord beroende på om det är "kronor" eller "lappar" fixade även så att den skiljer mellan plural  och singular

--- Assignment 1/change.py
+++ Assignment 1/change.py
@@ -1,12 +1,19 @@
-price = 0
-pay = 0 #what the person payed with
+price = 372.38
+pay = 1000 #how muck person payd
 amount = 0 # the amount of bills needed of the current type
 bills =[1000, 500, 200, 100, 50, 20, 10, 5, 1]
-change = price-pay
+change = pay-price
+
 for i, bill in enumerate(bills):
-    amount = change//bill
+    amount = int(change//bill)
     change = change%bill
-    if bill>10:
-        print(f'{amount} stycken {bill}lapp.')
+    if amount != 1: #lite if satser för att printa rätt ord så att meningarna låter bra. behövs inte egentligen.
+        if bill > 10:
+            print(f'{amount} {bill}-lappar.')
+        else:
+            print(f'{amount} {bill}-kronor.')
     else:
-        print(f'{amount} stycken {bill}krona.')
+        if bill > 10:
+            print(f'{amount} {bill}-lapp.')
+        else:
+            print(f'{amount} {bill}-krona.')
