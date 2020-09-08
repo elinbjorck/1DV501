1f7d3c16e12c60c3e651121694c74e032cc7205f
Elin Björck <elinbjorck@gmail.com>
2020-09-08 13:18:11 +0200

    klar

--- oddpossitive.py
+++ oddpossitive.py
@@ -2,3 +2,14 @@ from random import randint
 number = randint(-10,10)
 
 print(f'The generated number is {number}')
+
+if number % 2 == 0:
+    print(f'{number} is even', end = ' ')
+else:
+    print(f'{number} is odd', end = ' ')
+
+
+if number < 0:
+    print('and negative')
+else:
+    print('and possitive')
