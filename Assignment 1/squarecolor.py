129c909cb04f3df050e339ce63af2c6c9c070d8a
Elin Björck <elinbjorck@gmail.com>
2020-09-09 09:44:16 +0200

    Lade till så att det funkar även om personen skriver bokstaven som versal.

--- Assignment 1/squarecolor.py
+++ Assignment 1/squarecolor.py
@@ -2,6 +2,10 @@ identifier = input('Enter a chess square identifyer (from a1 to h8)')
 letter = identifier[0]
 number = int(identifier[1]) #här i början kan man sätta in checkar för att kolla så att användaren skriver in en ruta som faktiskt finns på brädet. 
 
+#skulle kunna lägga in och testa så att användaren skriver in det på rätt format
+
+letter = letter.lower()  #ifall någon skulle skriva in versaler
+print(letter)
 letter_number = ord(letter)-ord('a')+1 # Will find the letters distance from a, will give numbers from 0 to 7, 
                                          #then we ad 1 to get numbers from 1 to 8
 
