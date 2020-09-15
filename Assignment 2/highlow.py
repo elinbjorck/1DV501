86f2289151bcb520fb89a4d737830c15bc25d39c
Elin Björck <elinbjorck@gmail.com>
2020-09-15 18:49:46 +0200

    Fick ändra hade inte läst uppgiften ordentligt.  Nu slutar efter 10 gissningar

--- Assignment 2/highlow.py
+++ Assignment 2/highlow.py
@@ -1,12 +1,11 @@
 from random import randint
 random_number = randint(1,101)
-guess = int(input("I'm thinking of a number between 1 and 100, guess: "))
-number_of_guesses = 1
+print("I'm thinking of a number between 1 and 100, take a guess!")
 
-while guess != random_number:
+for number_of_guesses in range(1,11):
+    guess=int(input(f'Guess {number_of_guesses}: '))
     if guess > random_number: print('To high')
-    else: print('To low')
-    guess=int(input('Guess again: '))
-    number_of_guesses += 1
+    elif guess < random_number: print('To low')
+    else: print(f'You got it right after {number_of_guesses} guesses! Well donne.'), exit(0)
 
-print(f'You got it right after {number_of_guesses} guesses! Well donne.')
+print('Over 10 guesses just give up')
