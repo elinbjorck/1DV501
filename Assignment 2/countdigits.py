b2ac9ea1958fd2146d4e062f96272e91cc73bab1
Elin Björck <elinbjorck@gmail.com>
2020-09-15 19:13:58 +0200

    Fixade testen lite till

--- Assignment 2/countdigits.py
+++ Assignment 2/countdigits.py
@@ -1,9 +1,16 @@
 try:
-    integer = input('Enter a large possitive integer(at least 4 digits): ')
-    length = len(integer)
-    if 0 > int(integer) or int(integer) != float( integer ) or length < 4: exit()
-   
-except: print('Please follow the instructions'), exit(0)
+    int_string = input('Enter a large possitive integer(at least 4 digits): ')
+    length = len(int_string)
+    if int(int_string) < 0:
+        print('Your number is not possitive')
+        exit(0)
+    if length < 4:
+        print("Your does'nt have enough digits")
+        exit(0)
+
+except ValueError as letter_or_decimal:
+    print('Not an integer')
+    exit(0)
 
 current_number = 0
 
@@ -12,7 +19,7 @@ even = 0
 zero = 0
 
 for i in range(0,length):
-    current_number = int(integer[i])
+    current_number = int(int_string[i])
     if current_number == 0: zero += 1
     elif current_number % 2 == 0: even += 1
     else: odd += 1
