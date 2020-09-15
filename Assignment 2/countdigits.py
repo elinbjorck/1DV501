4182f4ec639408ffd4f988d97eba3798da84fcd6
Elin Björck <elinbjorck@gmail.com>
2020-09-15 17:06:01 +0200

    gjort countdigits

---
+++ Assignment 2/countdigits.py
@@ -0,0 +1,19 @@
+try:
+    integer = input('Enter a large possitive integer(at least 4 digits): ')
+    length = len(integer)
+    current_number = 0
+
+    odd = 0
+    even = 0
+    zero = 0
+
+    if 0 > int(integer) or int(integer) != float( integer ) or length < 4: exit()
+    for i in range(0,length):
+        current_number = int(integer[i])
+        if current_number == 0: zero += 1
+        elif current_number % 2 == 0: even += 1
+        else: odd += 1
+    print(f'Zeros: {zero}\nOdd: {odd}\nEven: {even}')
+
+except: print('Please follow the instructions')
+
--- Assignment 2/largest_k.py
+++ Assignment 2/largest_k.py
@@ -10,7 +10,7 @@ try:
         sigma += k
         k += 2
 
-    k -= 4 # while loopen avbryts när summan blir större än integer. det skedde när k var två stärre än största k, efter det plussas det på ytterligare 2 på k. 
+    k -= 4 # while loopen avbryts när summan blir större än integer. det skedde när k var två stärre än största-k, efter det plussas det på ytterligare 2 på k. 
     print(f'Largest k = {k}')
 
 except: print('Please follow the instructions')
