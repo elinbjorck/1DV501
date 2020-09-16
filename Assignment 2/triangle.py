4b62a9b5d704174c16b6c529eff28f1d182e0ab0
Elin Björck <35378839+elinbjorck@users.noreply.github.com>
2020-09-16 13:58:47 +0200

    klar med triangles

--- Assignment 2/triangle.py
+++ Assignment 2/triangle.py
@@ -1,5 +1,4 @@
 try:
-    print('Hi')
     number = int(input('Ge mig ett udda heltal som är possitivt: '))
 except: print('Följ instruktionerna ditt as.'), exit(0)
 
@@ -11,3 +10,10 @@ if number % 2 == 0:
     exit(0)
 
 print('wow, du är bra på att göra som du blir tillsagd')
+
+for i in range(number):
+    print(' '*i+'*'*(number-i))
+
+for i in range (1,number+1,2):
+    space = (number-i)//2
+    print(' '*space + '*'*i + ' '*space)
