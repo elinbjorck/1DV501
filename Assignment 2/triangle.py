6458ef9792f4224a41915bd245f5591f0e9952d7
Elin Björck <35378839+elinbjorck@users.noreply.github.com>
2020-09-16 13:43:55 +0200

    Lagt till passiv och aktiv aggresivitet.

--- Assignment 2/triangle.py
+++ Assignment 2/triangle.py
@@ -1,3 +1,13 @@
 try:
     print('Hi')
+    number = int(input('Ge mig ett udda heltal som är possitivt: '))
 except: print('Följ instruktionerna ditt as.'), exit(0)
+
+if number < 0:
+    print(f'{number} är inte possitivt.')
+    exit(0)
+if number % 2 == 0:
+    print(f'{number} är ett jamnt tal')
+    exit(0)
+
+print('wow, du är bra på att göra som du blir tillsagd')
