37853254a5eda208b2677526ea75d3ee214607f8
Elin Björck <elinbjorck@gmail.com>
2020-09-17 21:21:02 +0200

    klar med andra.

--- Assignment 2/list_functions.py
+++ Assignment 2/list_functions.py
@@ -6,6 +6,11 @@ def random_list(n):
         lst.append(randint(1,100))
     return lst
 def average(lst):
+    s = 0
+    for n in lst:
+        s += n
+    return round(s/len(lst))
+    
 def only_odd(lst):
 def to_string(lst):
 def contains(lst, a, b):
