c054815455d00d802895013b09c0f9acd2391f40
Elin Björck <elinbjorck@gmail.com>
2020-09-17 17:01:22 +0200

    klar med tärningarna.

---
+++ Assignment 2/two_dice.py
@@ -0,0 +1,12 @@
+
+from random import randint
+
+def throwdice():
+    return randint(1,6)
+
+ressult = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+for i in range(10000):
+    dice_sum = throwdice()+throwdice()
+    ressult[dice_sum-2] += 1
+
+print(ressult)
