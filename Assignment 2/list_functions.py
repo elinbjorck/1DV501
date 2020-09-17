d78486dc99fd509f37c8e0246a57206362799451
Elin Björck <elinbjorck@gmail.com>
2020-09-17 21:19:04 +0200

    klar med första, kan ge för få element i listan men lätt att åtgärda.

--- Assignment 2/list_functions.py
+++ Assignment 2/list_functions.py
@@ -1,4 +1,10 @@
+from random import randint
+
 def random_list(n):
+    lst = []
+    for i in range(n):
+        lst.append(randint(1,100))
+    return lst
 def average(lst):
 def only_odd(lst):
 def to_string(lst):
