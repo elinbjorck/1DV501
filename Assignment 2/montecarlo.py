bfdc4f2581cc6e511b3156c61876fb06b21a7f55
Elin Björck <elinbjorck@gmail.com>
2020-09-17 15:55:39 +0200

    klar med pi uppskattning

---
+++ Assignment 2/montecarlo.py
@@ -0,0 +1,22 @@
+from random import uniform
+from math import sqrt
+x_min, x_max = -1, 1
+y_min, y_max = -1, 1
+
+def random_point2D():
+    return [uniform(x_min, x_max), uniform(y_min,y_max)]
+
+def distance(point_a, point_b):
+    return sqrt((point_a[0]-point_b[0])**2 + (point_a[1]-point_b[1])**2)
+
+origo = [0, 0]
+points_inside = 0
+points_total = 0
+number_of_itterations = 10000
+
+for i in range(number_of_itterations):
+    if distance(random_point2D(), origo) <= 1:
+        points_inside += 1
+    points_total += 1
+
+print(4*points_inside/points_total)
