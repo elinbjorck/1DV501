e6f53b24eded30b2717630f748c3743f35ed4fbf
Elin Björck <elinbjorck@gmail.com>
2020-09-17 21:23:54 +0200

    klar med 3

--- Assignment 2/list_functions.py
+++ Assignment 2/list_functions.py
@@ -6,12 +6,18 @@ def random_list(n):
         lst.append(randint(1,100))
     return lst
 def average(lst):
-    s = 0
+    sum_ = 0
     for n in lst:
-        s += n
-    return round(s/len(lst))
-    
+        sum_ += n
+    return round(sum_/len(lst))
+
 def only_odd(lst):
+    odd_list = []
+    for n in lst:
+        if n % 2 == 1:
+            odd_list.append(n)
+    return odd_list
+
 def to_string(lst):
 def contains(lst, a, b):
 def has_duplicates(lst):
