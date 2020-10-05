4a4f3cc3233f37059d2499f99e8aea0dea6fbda7
Elin Björck <35378839+elinbjorck@users.noreply.github.com>
2020-10-05 11:08:00 +0200

    gjort alla funktioner. kanske inte gör på bästa sätt i count occurances

--- eb222wb_assign3/count_numbers.py
+++ eb222wb_assign3/count_numbers.py
@@ -1,3 +1,5 @@
+import os
+
 PATH_FILE_A = os.getcwd()+'/eb222wb_assign3/10000_integers/file_10000integers_A.txt'
 PATH_FILE_B = os.getcwd()+'/eb222wb_assign3/10000_integers/file_10000integers_B.txt'
 
@@ -10,9 +12,19 @@ def file_to_int_list(path, separator):
     return int_list
 
 def count_different(lst):
-    pass
+    number_set = set(lst)
+    print(len(number_set))
 
 def count_ocurances(lst):
-    pass
+    ocurances = {}
+    for number in lst:
+        ocurances[str(number)] = 0
+    for number in lst:
+        ocurances[str(number)] += 1
+    print(ocurances)
+
 
 test_list = [1, 3 , 6, 6, 7, 8, 4, 4]
+
+count_different(test_list)
+count_ocurances(test_list)
