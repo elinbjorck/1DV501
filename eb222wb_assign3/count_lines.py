d214a795f2d0187dfa80622907199146c37058e3
Elin Björck <35378839+elinbjorck@users.noreply.github.com>
2020-10-05 10:27:59 +0200

    gjort klart lite till

--- eb222wb_assign3/count_lines.py
+++ eb222wb_assign3/count_lines.py
@@ -1,23 +1,26 @@
 import os
 
 path_current = os.getcwd()
-
 print(path_current)
 
-itterator_thing = os.scandir(path_current)
-path_list =[]
-for thing in itterator_thing:
-    print(thing)
-    if thing.is_dir():
-        path_list.append(thing.path)
-print(path_list)
+def count_lines(file_path):
+    lines = 0
+    with open(file_path,'r',encoding='utf8') as python_file:
+        for line in python_file:
+            if line.strip():
+                lines += 1
+    return lines
 
 def dir_digger(path):
     directory_content = os.scandir(path)
+    total_lines = 0
     for content in directory_content:
-        if content.is_dir:
-            dir_digger(content.path)
+        if content.name.startswith('.'):
+            continue
+        elif content.is_dir():
+            total_lines += dir_digger(content.path)
         elif content.name.endswith('.py'):
-            print(content.name)
+            total_lines += count_lines(content.path)
+    return total_lines
 
-dir_digger(path_current)
+print(dir_digger(path_current))
--- eb222wb_assign3/read_numbers.py
+++ eb222wb_assign3/read_numbers.py
@@ -1,6 +1,11 @@
 from math import sqrt
 import os
 
+PATH_FILE_A = os.getcwd()+'/eb222wb_assign3/10000_integers/file_10000integers_A.txt'
+PATH_FILE_B = os.getcwd()+'/eb222wb_assign3/10000_integers/file_10000integers_B.txt'
+
+print(PATH_FILE_A, PATH_FILE_B)
+
 def mean(lst):
     _sum = 0
     for i in lst:
@@ -15,10 +20,19 @@ def std(lst):
         _sum += (i-mean_value)**2
     return sqrt(_sum/len(lst))
 
+def file_to_int_list(path, separator):
+    int_list = []
+    with open(path, 'r') as file_:
+        for line in file_:
+            for i in line.split(separator):
+                int_list.append(int(i))
+    return int_list
 
-test_list = [1,6,5,7,4,3,9]
-print(mean(test_list))
-print(std(test_list))
-
+A_list = file_to_int_list(PATH_FILE_A,',')
+B_List = file_to_int_list(PATH_FILE_B,':')
+print(mean(A_list))
+print(std(A_list))
+print(mean(B_List))
+print(std(B_List))
 path = os.getcwd()
 print(path)
